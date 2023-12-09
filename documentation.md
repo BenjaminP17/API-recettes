0c03bf38919694c3a2cb9ecb70a62b285b213e27
Benjamin Pelissier <benjpls@gmail.com>
2023-12-09 16:01:46 +0100

    install du bundle serializer

--- documentation.md
+++ documentation.md
@@ -5,3 +5,13 @@ Bundles installés :
 
 - maker-bundle (pour créer nos controllers via le CLI)
 - ORM Pack
+- serializer-pack
+
+
+Liste des routes : 
+
+----------------  -------  -------  -----  --------------------------------------------
+Name              Method   Scheme   Host   Path
+----------------  -------  -------  -----  --------------------------------------------
+all recipes         GET      ANY     ANY   /recipes
+
--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -2,18 +2,22 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use App\Repository\RecipeRepository;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
 
 class RecipeController extends AbstractController
 {
-    #[Route('/recipe', name: 'app_recipe')]
-    public function index(): JsonResponse
+    #[Route('/api/recipes', name: 'app_recipe', methods: ['GET'])]
+    public function getAllRecipe(RecipeRepository $RecipeRepository, SerializerInterface $serializer): JsonResponse
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/RecipeController.php',
+        $recipesList = $RecipeRepository->findall();
+        return new JsonResponse([
+            'recipes' => $recipesList,
+            
         ]);
     }
 }
