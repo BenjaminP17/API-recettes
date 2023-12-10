67a7528b2f886ea902c30224ad0de1044d47301b
Benjamin Pelissier <benjpls@gmail.com>
2023-12-10 09:26:32 +0100

    creation du controller recette et route de toutes les recettes

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -3,11 +3,13 @@
 namespace App\Controller;
 
 use App\Repository\RecipeRepository;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+// Route qui renvoi la liste de toutes les recettes (titre et description)
 
 class RecipeController extends AbstractController
 {
@@ -15,9 +17,9 @@ class RecipeController extends AbstractController
     public function getAllRecipe(RecipeRepository $RecipeRepository, SerializerInterface $serializer): JsonResponse
     {
         $recipesList = $RecipeRepository->findall();
-        return new JsonResponse([
-            'recipes' => $recipesList,
-            
-        ]);
+
+        $jsonRecipesList = $serializer->serialize($recipesList, 'json');
+
+        return new JsonResponse([$jsonRecipesList, Response::HTTP_OK, [], true]);
     }
 }
